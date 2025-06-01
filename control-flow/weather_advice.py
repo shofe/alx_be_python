df0c860cee56f4d73a09a79302fa5c559e671b89
shofe <tayoshofunlayo@gmail.com>
2025-06-01 23:44:07 +0100

    This is a weather advice code

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+#Asking user for input on their weather condition
+user_input = input("What is the weather like today? \n (sunny/rainy/cold) ")
+
+#Using user"s input to give advice by implementig IF statements.
+if user_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif user_input =="rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif user_input == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
