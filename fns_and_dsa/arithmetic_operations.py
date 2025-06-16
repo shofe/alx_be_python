c4ba31eb0eafc278288398b25a36f6c3585519fb
shofe <tayoshofunlayo@gmail.com>
2025-06-16 03:04:36 +0100

    This is an arithmetic operator

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Cannot divide by zero."
-            return num1 / num2
+            else:
+                return num1 / num2
         case "multiply":
             return num1 * num2
         case _:
