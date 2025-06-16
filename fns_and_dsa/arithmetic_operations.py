cc9051e5072789b36c2199a717ca508713b36481
shofe <tayoshofunlayo@gmail.com>
2025-06-16 03:08:13 +0100

    This is an arithmetic calculator

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,31 @@
-#defining a function to perform arthimetic operations.
+# #defining a function to perform arthimetic operations.
+# def perform_operation(num1, num2, operation):
+#     match operation:
+#         case "add":
+#             return num1 + num2
+#         case "subtract":
+#             return num1 - num2
+#         case "divide":
+#             if num2 == 0:
+#                 return "Error: Cannot divide by zero."
+#             else:
+#                 return num1 / num2
+#         case "multiply":
+#             return num1 * num2
+#         case _:
+#             print("Invalid inputs")
+
+#OR
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "divide":
-            if num2 == 0:
-                return "Error: Cannot divide by zero."
-            else:
-                return num1 / num2
-        case "multiply":
-            return num1 * num2
-        case _:
-            print("Invalid inputs")
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cannot divide by zero."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation."
