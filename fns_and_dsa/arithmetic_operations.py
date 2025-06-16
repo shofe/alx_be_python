c0e2b20e0aee9527be4fac9e8f8b821716078bdd
shofe <tayoshofunlayo@gmail.com>
2025-06-16 02:24:59 +0100

    This is an arithmetic operator

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+#defining a function to perform arthimetic operations.
+def perform_operation(num1, num2,operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "divide":
+            if num2 == 0:
+                print("please another number")
+                return None
+            #return num1 / num2
+        case "multiply":
+            return num1 * num2
+        case _:
+            print("Invalid inputs")
