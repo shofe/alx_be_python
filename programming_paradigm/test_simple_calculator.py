0a9ea70734cae75ec3c7bf4083dfbf0433f017c6
shofe <tayoshofunlayo@gmail.com>
2025-06-28 15:31:52 +0100

    This is the code i wrote and it test the given code to ensure therr are no issues and also check for ZeroDivision.

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,48 @@
+import unittest    #importing the in-built unittest module from python
+
+from simple_calculator import SimpleCalculator   #importing the simpleClculator from simple_calculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self): 
+        """Set up the SimpleCalculator instance before each test."""
+
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+
+        """Test the addition method."""
+
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtraction(self):
+
+        #Testing the subtraction method
+
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+
+        self.assertEqual(self.calc.subtract(70, 3), 67)
+
+
+    def test_multiply(self):
+
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+
+    def test_divide(self):
+            
+            
+            self.assertEqual(self.calc.divide(2, 0), None)
+
+            self.assertEqual(self.calc.divide(6, 2), 3)
+            
+                 
+        
+
+
+
+
+
