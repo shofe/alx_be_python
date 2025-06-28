3de8fd03ce95d17ff7fa7dda6820b4d424929649
shofe <tayoshofunlayo@gmail.com>
2025-06-28 15:36:14 +0100

    I changed divide to division in my unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -32,7 +32,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
         self.assertEqual(self.calc.multiply(2, 3), 6)
 
-    def test_divide(self):
+    def test_division(self):
             
             
             self.assertEqual(self.calc.divide(2, 0), None)
