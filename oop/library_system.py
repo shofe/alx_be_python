7bd73262e9e67cc863f4c906541d9d1791707657
shofe <tayoshofunlayo@gmail.com>
2025-07-13 17:08:58 +0100

    Updated the code to add the right output

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,14 +1,11 @@
-# Base Class
+# Base Class: Book
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
-        return f"Title: {self.title}, Author: {self.author}"
-    
     def __str__(self):
-        return self.get_details
+        return f"Book: {self.title} by {self.author}"
 
 
 # Derived Class: EBook
@@ -17,12 +14,9 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def __str__ (self):
-        return f"{super().get_details()}, File Size: {self.file_size}MB"
-
-
     def __str__(self):
-        return self.get_details
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 # Derived Class: PrintBook
 class PrintBook(Book):
@@ -31,29 +25,26 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().get_details()}, Pages: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-
-    def __str__(self):
-        return self.get_details
 
 # Composition Class: Library
 class Library:
     def __init__(self):
-        self.books = []  # stores instances of Book, EBook, and PrintBook
+        self.books = []
 
     def add_book(self, book):
         if isinstance(book, Book):
             self.books.append(book)
             print("A book has been added.")
         else:
-            print("Only instances of Book or its subclasses can be added.")
+            print("Invalid book type.")
 
     def list_books(self):
         if not self.books:
             print("No books in the library.")
         else:
-            print("\nLibrary Collection:")
+            print("Library Collection:")
             for book in self.books:
-                print(book.get_details())
+                print(book)
 
