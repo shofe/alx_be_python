23c1ee8413f4ec9faf0dde9a306b1b85698dccaa
shofe <tayoshofunlayo@gmail.com>
2025-07-16 14:22:59 +0100

    made a change in the class method

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,7 +2,7 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
 
     @classmethod
-    def multiply (cls ,a , b):
+    def multiply(cls ,a , b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
 
