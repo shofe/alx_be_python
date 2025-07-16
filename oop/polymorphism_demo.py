9868476192a1eba7020196954209c83e375fcd4c
shofe <tayoshofunlayo@gmail.com>
2025-07-16 14:05:42 +0100

    I changed the radius formula

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -18,7 +18,8 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius*self.radius
+        return math.pi * self.radius ** 2
+
 
                 
     
